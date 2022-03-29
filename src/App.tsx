b8c94d8b14ab4bdb6ba0b77762dc2b8494722637
olena <melolena33@gmail.com>
2022-03-29 12:36:17 +0300

    Get info

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,49 @@ import './App.scss';
 import UAParser from 'ua-parser-js';
 
 const getDeviceInfo = () => {
-  const { userAgent } = navigator;
-
+  // const { userAgent } = navigator;
+  const infoSections = [];
   const parser = new UAParser();
 
-  parser.setUA(userAgent);
-  const result = parser.getResult();
+  const userOS = parser.getOS();
+  const userDevice = parser.getDevice();
+  const userBrowser = parser.getBrowser();
+
+  if (userOS && userOS.name && userOS.version) {
+    infoSections.push({ name: 'OS', value: `${userOS.name} ${userOS.version}` });
+  }
+
+  if (userDevice && userDevice.vendor && userDevice.model) {
+    infoSections.push({ name: 'Device', value: `${userDevice.vendor} ${userDevice.model}` });
+  } else {
+    infoSections.push({ name: 'Device', value: 'N/A' });
+  }
+
+  if (userBrowser && userBrowser.name && userBrowser.version) {
+    infoSections.push({ name: 'Browser', value: `${userBrowser.name} ${userBrowser.version}` });
+  }
+
+  if (window) {
+    if (window.screen) {
+      infoSections.push({ name: 'Screen resolution', value: `${window.screen.width} x ${window.screen.height}` });
+      infoSections.push({ name: 'Available screen space', value: `${window.screen.availWidth} x ${window.screen.availHeight}` });
+    }
+
+    infoSections.push({ name: 'Browser width size', value: `${window.innerWidth} x ${window.innerHeight}` });
+    infoSections.push({ name: 'Device pixel ratio', value: window.devicePixelRatio });
+  }
+
+  // parser.setUA(userAgent);
+  // const result = parser.getResult();
 
   // eslint-disable-next-line no-console
-  console.log('*** result', result);
-  const deviceType = (result.device && result.device.type) || 'desktop';
+  console.log('*** infoSections', infoSections);
+  // const deviceType = (result.device && result.device.type) || 'desktop';
 
   // eslint-disable-next-line no-console
-  console.log('*** device type', deviceType);
+  // console.log('*** device type', deviceType);
 
-  return { deviceType };
+  return infoSections;
 };
 
 export const App: React.FC = () => {
